ecfcde0399fd5ed10cbb7c0618a8cbc08a772ad0
Soham K <sohamk10@gmail.com>
2020-04-23 23:09:51 +0530

    Completed Modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,11 +2,13 @@ import './../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
-let stickyHeader = new StickyHeader()
+new StickyHeader()
+new Modal()
 
 if (module.hot) {
   module.hot.accept()
