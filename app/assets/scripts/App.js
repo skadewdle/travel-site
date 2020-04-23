b0a9f27157df4ebe72207402aad64a73046c2885
Soham K <sohamk10@gmail.com>
2020-04-23 11:46:09 +0530

    Implemented revealing on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import './../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new MobileMenu()
+new RevealOnScroll(document.querySelectorAll('.feature'), 75)
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 
 if (module.hot) {
   module.hot.accept()
 }
-
-let mobileMenu = new MobileMenu()
