fcb96b741721af9cef5fd51d3f4bcc985888c314
Soham K <sohamk10@gmail.com>
2020-04-24 12:05:44 +0530

    Completed create-modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,30 @@ import './../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-import Modal from './modules/Modal'
+
 
 new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 new StickyHeader()
-new Modal()
+let modal
+
+document.querySelectorAll('.open-modal').forEach(cur => {
+  cur.addEventListener('click' ,e => {
+    e.preventDefault()
+    if (typeof(modal) == 'undefined') {
+      import (/* webpackChunkName: "modal" */'./modules/Modal').then(Modal => {
+        modal = new Modal.default()
+        setTimeout(() => {
+          modal.openModal()  
+        }, 20);
+      }).catch(err => console.log(err))
+    }
+    else {
+      modal.openModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
